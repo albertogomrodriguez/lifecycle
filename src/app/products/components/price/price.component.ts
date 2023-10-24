8376cc876b5e151f4a7bb59b48a6262c5e9080eb
Alberto Gómez <alberto.grodriguez@bosonit.com>
2023-10-24 13:09:15 +0200

    Sección 20. Ciclos de vida terminados

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -6,6 +6,7 @@ import {
   SimpleChanges,
   Input,
 } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -15,9 +16,11 @@ import {
 export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price: number = 0;
+  public interval$?:Subscription
 
   ngOnInit(): void {
     console.log('Componente hijo: ngOnInit');
+    this.interval$= interval (1000).subscribe((value) => console.log(`Tick:${value}`));
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Componente hijo: ngOnChanges');
@@ -25,5 +28,6 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   }
   ngOnDestroy(): void {
     console.log('Componente hijo: ngOnDestro');
+    this.interval$?.unsubscribe()
   }
 }
